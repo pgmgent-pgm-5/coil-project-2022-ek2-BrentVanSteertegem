7d6d1bdb99a6b717ca274f5e3fd25a6df8bd13ac
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-07-04 19:53:21 +0200

    chore: Renamed index.css to reset.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'unfonts.css'
-import './style/index.css'
+import './style/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>

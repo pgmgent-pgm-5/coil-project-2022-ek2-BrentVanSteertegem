3e6fd1cb27344b9f6e8a049a4ac1be95a405a0a9
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-07-06 13:59:52 +0200

    feat: Added design system

---
+++ frontend/src/style/index.ts
@@ -0,0 +1,44 @@
+const Colors = {
+    white: '#fff',
+    black: '#000',
+    red: '#e00',
+    yellow: '#fe0',
+}
+  
+const Fonts = {
+    text: 'Poppins',
+    logo: 'Legothick',
+}
+
+const Sizes = {
+    medium: 1,
+    container: 80,
+}
+  
+const TextSizes = {
+    medium: Sizes.medium,
+}
+  
+const Spacing = {
+    medium: Sizes.medium,
+}
+  
+const Rounded = {
+    medium: Sizes.medium,
+}
+  
+export const Variables = {
+    colors: { 
+        ...Colors,
+        primary: Colors.red,
+        secondary: Colors.yellow,
+        text: Colors.black,
+        logoText: Colors.white,
+        buttonText: Colors.white,
+    },
+    fonts: { ...Fonts },
+    sizes: { ...Sizes },
+    textSizes: { ...TextSizes },
+    spacing: { ...Spacing },
+    rounded: { ...Rounded },
+}

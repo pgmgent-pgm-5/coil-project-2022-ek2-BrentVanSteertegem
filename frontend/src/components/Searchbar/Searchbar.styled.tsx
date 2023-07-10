dc71d49a2933a8d26792431d75bd18169f899356
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-07-10 18:15:08 +0200

    chore: Updated Searchbar styling

--- frontend/src/components/Searchbar/Searchbar.styled.tsx
+++ frontend/src/components/Searchbar/Searchbar.styled.tsx
@@ -10,19 +10,22 @@ export const StForm = styled.form`
     background-color: ${Variables.colors.white};
     border-radius: 0.25rem;
     margin: 1rem 0;
+    align-items: center;
 `
 
 export const StSearchbar = styled(StInputfield)`
     border-radius: 0.25rem 0 0 0.25rem;
     margin: 0;
+    padding-right: 0;
 `
 
 export const StSearchButton = styled.button`
-    padding: 0.55rem 0.75rem 0.45rem 0;
+    padding: 0.5rem 0.75rem 0.5rem 0.5rem;
     border: 0;
     border-radius: 0 0.25rem 0.25rem 0;
     margin: 0;
     font-size: 0.9rem;
+    line-height: 0.9rem;
     background-color: ${Variables.colors.white};
     color: ${Variables.colors.placeHolder};
 `

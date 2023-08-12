b681f3c39d983281864450904825a5ce5e387ecf
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-08-12 16:10:09 +0200

    chore: Made sure the page scrolls to top every time another page is loaded

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { Outlet } from 'react-router-dom'
 
 // Add all icons to library so they can be used dynamically
 library.add(fas, fab)
 
 const App = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+}, [location])
+
   return (
     <Outlet />
   )

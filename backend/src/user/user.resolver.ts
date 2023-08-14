6c147dc3322d0393d0c0c64b58c86f17d6b63e71
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-08-14 22:35:41 +0200

    fix: Updated import of User entity in UserService

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
 import { UserService } from './user.service'
 import { CreateUserInput } from './dto/create-user.input'
 import { UpdateUserInput } from './dto/update-user.input'
-import { User } from './entities/User.entity'
+import { User } from './entities/user.entity'
 
 @Resolver(() => User)
 export class UserResolver {

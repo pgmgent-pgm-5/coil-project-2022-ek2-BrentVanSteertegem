ab2e3a9ebde3ec6611134e39077dfa44af4f8d49
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-08-16 15:47:34 +0200

    chore: Updated whitespace in Hero

--- frontend/src/components/Hero/Hero.styled.tsx
+++ frontend/src/components/Hero/Hero.styled.tsx
@@ -3,10 +3,14 @@ import { Variables } from '../../style'
 
 export const StHeroSection = styled.section`
     background-color: ${Variables.colors.lightGrey};
-    padding: ${Variables.spacing.medium}rem;
+    padding: ${Variables.spacing.large}rem;
     border-radius: ${Variables.rounded.medium}rem;
     align-items: center;
     margin: ${Variables.spacing.medium}rem 0;
+
+    h1, h2, h3, h4, h5, h6 {
+        margin-top: 0;
+    }
 `
 
 export const StHeroImage = styled.img`

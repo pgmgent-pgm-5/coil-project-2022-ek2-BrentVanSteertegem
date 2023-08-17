b581ed43cc50fb94b7737868b0fea379f397045c
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-08-17 20:54:39 +0200

    feat: Added Order seeding

--- backend/src/seeder/factories/Orderfactory.ts
+++ backend/src/seeder/factories/Orderfactory.ts
@@ -3,6 +3,7 @@ import { appDataSource } from '../Seed'
 import { CreateOrderInput } from '../../order/dto/create-order.input'
 import { Order } from '../../order/entities/order.entity'
 import { ShippingMethod } from '../../shippingMethods.enum'
+import { faker } from '@faker-js/faker'
 
 class OrderFactory extends Factory {
   orders: CreateOrderInput[]
@@ -19,34 +20,69 @@ class OrderFactory extends Factory {
 
   // make many records
   async makeMany() {
-    this.setOrders()
+    this.orders = await this.generateOrders(248)
     this.orders.forEach(async (order) => {
       const record = await this.insert(order)
       this.inserted.push(record)
     })
   }
 
-  setOrders = () => {
+  generateOrders = async (amount: number) => {
+    const orders = []
     const brickRepository = appDataSource.getRepository('Brick')
-    const bricks = brickRepository.find()
+    const bricks = await brickRepository.find()
+    const shippingMethods = Object.values(ShippingMethod)
 
-    this.orders.push({
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john@doe.com',
-      street: 'Main Street',
-      houseNumber: '1',
-      zipCode: '12345',
-      city: 'Berlin',
-      shippingMethod: ShippingMethod.STANDARD,
-      shippingPrice: 4.99,
-      cardNumber: '1234123412341234',
-      cardExpirationMonth: 12,
-      cardExpirationYear: 2023,
-      cardSecurityCode: '123',
-      total: 5.19,
-      items: [JSON.stringify({ amount: 2, brick: bricks[0] })],
-    })
+    for (let i = 0; i < amount; i++) {
+      const firstName = faker.person.firstName()
+      const lastName = faker.person.lastName()
+      const shippingMethod =
+        shippingMethods[Math.floor(Math.random() * shippingMethods.length)]
+      const shippingPrice =
+        shippingMethod === ShippingMethod.PICKUP
+          ? 3.99
+          : shippingMethod === ShippingMethod.STANDARD
+          ? 4.99
+          : 5.99
+
+      let totalPrice = shippingPrice
+
+      const amountOfCartItems = Math.floor(Math.random() * 6) + 1
+      const cartItems = []
+      const addedBrickIds = []
+      for (let i = 0; i < amountOfCartItems; i++) {
+        let brick = bricks[Math.floor(Math.random() * bricks.length)]
+        while (addedBrickIds.includes(brick.id)) {
+          brick = bricks[Math.floor(Math.random() * bricks.length)]
+        }
+        const amount = Math.floor(Math.random() * 4) + 1
+        totalPrice += amount * brick.price
+        cartItems.push(JSON.stringify({ amount: amount, brick: brick }))
+      }
+
+      orders.push({
+        firstName: firstName,
+        lastName: lastName,
+        email: faker.internet.email({
+          firstName: firstName,
+          lastName: lastName,
+        }),
+        street: faker.location.street(),
+        houseNumber: faker.location.buildingNumber(),
+        zipCode: faker.location.zipCode(),
+        city: faker.location.city(),
+        shippingMethod: shippingMethod,
+        shippingPrice: shippingPrice,
+        cardNumber: `${faker.finance.creditCardNumber()}`,
+        cardExpirationMonth: Math.floor(Math.random() * 12) + 1,
+        cardExpirationYear: Math.floor(Math.random() * 4) + 2024,
+        cardSecurityCode: `${faker.finance.creditCardCVV()}`,
+        total: totalPrice,
+        items: [JSON.stringify({ amount: 2, brick: bricks[0] })],
+      })
+    }
+
+    return orders
   }
 
   async insert(order: CreateOrderInput) {
